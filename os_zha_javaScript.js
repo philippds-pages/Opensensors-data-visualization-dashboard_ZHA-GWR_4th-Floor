f8fdf95e2f20eb7d4c60e79f2208c201e9cb0735
Philipp Siedler <p.d.siedler@gmail.com>
2018-01-18 19:48:48 +0000

    color link bugg fix

--- os_zha_javaScript.js
+++ os_zha_javaScript.js
@@ -2,12 +2,16 @@ function changeColor(id) {
 
   if(id == "i0") { document.getElementById("i0").style.color = "red"; }
   else { document.getElementById("i0").style.color = "black"; }
+
   if(id == "i1") { document.getElementById("i1").style.color = "red"; }
   else { document.getElementById("i1").style.color = "black"; }
+
   if(id == "i2") { document.getElementById("i2").style.color = "red"; }
   else { document.getElementById("i2").style.color = "black"; }
+
   if(id == "i3") { document.getElementById("i3").style.color = "red"; }
   else { document.getElementById("i3").style.color = "black"; }
+
   if(id == "i4") { document.getElementById("i4").style.color = "red"; }
   else { document.getElementById("i4").style.color = "black"; }
 
