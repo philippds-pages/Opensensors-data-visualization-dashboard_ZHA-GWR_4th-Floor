c743f9b5801b61956cb2bd041964ad8502d54b90
Philipp Siedler <p.d.siedler@gmail.com>
2018-01-18 19:46:20 +0000

    class change expl 0 to i0

--- os_zha_javaScript.js
+++ os_zha_javaScript.js
@@ -1,44 +1,39 @@
 function changeColor(id) {
-  document.getElementById("0").style.color = "black";
-  document.getElementById("1").style.color = "black";
-  document.getElementById("2").style.color = "black";
-  document.getElementById("3").style.color = "black";
-  document.getElementById("4").style.color = "black";
 
-  if(id == "0") { document.getElementById("0").style.color = "red"; }
-  else { document.getElementById("0").style.color = "black"; }
-  if(id == "1") { document.getElementById("1").style.color = "red"; }
-  else { document.getElementById("1").style.color = "black"; }
-  if(id == "2") { document.getElementById("2").style.color = "red"; }
-  else { document.getElementById("2").style.color = "black"; }
-  if(id == "3") { document.getElementById("3").style.color = "red"; }
-  else { document.getElementById("3").style.color = "black"; }
-  if(id == "4") { document.getElementById("4").style.color = "red"; }
-  else { document.getElementById("4").style.color = "black"; }
+  if(id == "i0") { document.getElementById("i0").style.color = "red"; }
+  else { document.getElementById("i0").style.color = "black"; }
+  if(id == "i1") { document.getElementById("i1").style.color = "red"; }
+  else { document.getElementById("i1").style.color = "black"; }
+  if(id == "i2") { document.getElementById("i2").style.color = "red"; }
+  else { document.getElementById("i2").style.color = "black"; }
+  if(id == "i3") { document.getElementById("i3").style.color = "red"; }
+  else { document.getElementById("i3").style.color = "black"; }
+  if(id == "i4") { document.getElementById("i4").style.color = "red"; }
+  else { document.getElementById("i4").style.color = "black"; }
 
 }
 
 function changeImage0(a) {
     document.getElementById("img").src=a;
-    changeColor("0");
+    changeColor("i0");
 }
 
 function changeImage1(a) {
     document.getElementById("img").src=a;
-    changeColor("1");
+    changeColor("i1");
 }
 
 function changeImage2(a) {
     document.getElementById("img").src=a;
-    changeColor("2");
+    changeColor("i2");
 }
 
 function changeImage3(a) {
     document.getElementById("img").src=a;
-    changeColor("3");
+    changeColor("i3");
 }
 
 function changeImage4(a) {
     document.getElementById("img").src=a;
-    changeColor("4");
+    changeColor("i4");
 }
